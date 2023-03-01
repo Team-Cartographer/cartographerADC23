d21f1efd8285b35b5133c092870be55d2b637753
abhi-arya1 <abhia9167@gmail.com>
2023-02-28 20:57:20 -0800

    Updated Spacing/Types on Parser/Display

--- Parser.py
+++ Parser.py
@@ -2,22 +2,23 @@ from PIL import Image
 from math import sqrt
 
 color_temps = {
-'K26_30' : (249, 218, 241),
-'K31_40' : (205, 109, 243),
-'K41_50' : (178, 107, 242),
-'K51_60' : (141, 105, 168),
-'K61_70' : (106, 123, 167),
-'K71_80' : (134, 145, 243),
-'K81_90' : (179, 208, 250),
-'K91_100' : (149, 210, 246),
-'K101_110' : (144, 247, 248),
-'K111_200' : (255, 255, 255),
-'K201_225' : (255, 254, 226),
-'K226_250' : (255, 255, 146),
-'K251_275' : (248, 215, 137),
-'K276_300' : (243, 174, 136),
-'K301_325' : (239, 135, 132),
-'K326_350' : (177, 130, 128)
+    # Key: K -> Kelvin, and Numbers -> Kelvin Temp. Range
+    'K26_30' : (249, 218, 241),
+    'K31_40' : (205, 109, 243),
+    'K41_50' : (178, 107, 242),
+    'K51_60' : (141, 105, 168),
+    'K61_70' : (106, 123, 167),
+    'K71_80' : (134, 145, 243),
+    'K81_90' : (179, 208, 250),
+    'K91_100' : (149, 210, 246),
+    'K101_110' : (144, 247, 248),
+    'K111_200' : (255, 255, 255),
+    'K201_225' : (255, 254, 226),
+    'K226_250' : (255, 255, 146),
+    'K251_275' : (248, 215, 137),
+    'K276_300' : (243, 174, 136),
+    'K301_325' : (239, 135, 132),
+    'K326_350' : (177, 130, 128)
 }
 
 
@@ -35,7 +36,6 @@ def getBestColor(pixel: tuple) -> tuple:
     return mincolor
 
 
-
 # Open the image
 image = Image.open('C:/Users/ashwa/Desktop/image.png')
 image = image.convert("RGB")
@@ -49,10 +49,6 @@ rgb_values = []
 for y in range(height):
     for x in range(width):
         # Get the RGB values of the pixel at (x, y)
-
-        if y == height//2:
-            print('Halfway There')
-            print("Woah!!! Livin' on a prayer!")
         color = image.getpixel((x, y))
         rgb_values.append(getBestColor(color))
 
