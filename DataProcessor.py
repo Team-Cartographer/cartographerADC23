065ee2d9bd4445351b94a42c79b1d55238cde042
abhi arya <abhia9167@gmail.com>
2023-03-01 16:45:21 -0800

    DataProcessor fixed to scale x, y, and z values to the minimum, and divide all x, y, z values by 40 for AStar Pathfinding.

--- DataProcessor.py
+++ DataProcessor.py
@@ -6,15 +6,12 @@ from numpy import cos, sin
 
 
 pathfile_path = fc.appfiles_path + '\Paths to Data.txt'
-with open(pathfile_path, mode="r") as f:
 
+with open(pathfile_path, mode="r") as f:
     paths = list(csv.reader(f, delimiter='\n'))
     for i in range(len(paths)):
         paths[i] = paths[i][0]
-
     f.close()
-
-
 
 DISTANCE_BETWEEN_POINTS = str(paths[4]).rstrip("\n")
 latitude_path = str(paths[0]).replace("\\", "/").rstrip("\n")
@@ -51,34 +48,35 @@ def generate_data_array():
     cols = len(longitude_list[0])
     xy_dim = len(longitude_list)
 
-    for row in range(rows):
-        for data_pt in range(cols):
-            # dataArray[k][0] = Lat, dA[k][1] = long, dA[k][2] = ht, dA[k][3] = slope
-            dataArray.append(
-                # TODO: Double Check the Correctness of this Statement.
-                [latitude_list[row][data_pt], longitude_list[row][data_pt], height_list[row][data_pt],
-                 slope_list[row][data_pt]]
-            )
+    # Change to Archive Path for final build.
+    dataArrayPath = fc.data_path + "/RawDataArray.csv"
 
-    return xy_dim
+    with open(dataArrayPath, mode="w", newline="") as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in range(rows):
+            for data_pt in range(cols):
+                # dataArray[k][0] = Lat, dA[k][1] = long, dA[k][2] = ht, dA[k][3] = slope
+                tmp = [latitude_list[row][data_pt], longitude_list[row][data_pt], height_list[row][data_pt],
+                    slope_list[row][data_pt]]
+                dataArray.append(tmp)
+                csv_writer.writerow(tmp)
+    f.close()
+
+
+    return xy_dim, dataArrayPath
 
 
 # Helper Functions for Math
 def get_x_coord(lat, long, rad):  # takes in degrees latitude and longitude
     return float(rad) * cos(float(lat)) * cos(float(long))
-
-
 def get_y_coord(lat, long, rad):
     return float(rad) * cos(float(lat)) * sin(float(long))
-
-
 def get_z_coord(lat, long, rad):  # long is technically not used here. I kept it for consistency. -JL
     return float(rad) * sin(float(lat))
 
-
 def write_rect_file(data_arr):
     rect_coord_path = fc.data_path + "/RectangularCoordinateData.csv"  # Processed Data Folder given from FolderCreator.py
-
+    xs, ys, zs, = [], [], []
     with open(rect_coord_path, mode="w", newline="") as datafile:
         csv_writer = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i in range(len(data_arr)):
@@ -86,17 +84,20 @@ def write_rect_file(data_arr):
             lat = data_arr[i][0]
             long = data_arr[i][1]
             height = data_arr[i][2]
-            slope = data_arr[i][3]
+            slope = float(data_arr[i][3])
             radius = lunar_rad + float(height)
 
-            x = get_x_coord(lat, long, radius)
-            y = get_y_coord(lat, long, radius)
-            z = get_z_coord(lat, long, radius)
+            x = float(get_x_coord(lat, long, radius))
+            y = float(get_y_coord(lat, long, radius))
+            z = float(get_z_coord(lat, long, radius))
 
             csv_writer.writerow([x, y, z, slope])
-        datafile.close()
+            xs.append(x), ys.append(y), zs.append(z)
+            zeroArray.append([x, y, z, slope])
 
-    return rect_coord_path
+        datafile.close()
+    min_x, min_y, min_z = abs(min(xs)), abs(min(ys)), abs(min(zs))
+    return rect_coord_path, min_x, min_y, min_z
 
 # TODO: finish astar data function
 def write_astar_data(data_arr):
@@ -109,73 +110,30 @@ def write_astar_data(data_arr):
     return astar_data_path
 
 
-# Deprecated
-def write_misc_file(min_height):
-    misc_path = fc.data_path + "/MiscData.csv"
-
-    with open(misc_path, mode="w", newline="") as f:
-        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([x_and_y_dim])  # Line 1
-        csv_writer.writerow([DISTANCE_BETWEEN_POINTS])  # Line 2
-        csv_writer.writerow([min_height])  # Line 3
-
-    f.close()
-    return misc_path
-
-
 # Helper Methods for Finding Maximums and Minimums of Each Attribute of <DataArray>
 def find_max_value(data_arr, attr):
-    return max(data_arr[attr])
+    return max(data_arr[attr], axis='columns')
 def find_min_value(data_arr, attr):
-    return min(data_arr[attr])
+    return min(data_arr[attr], axis='columns')
 
-
-
-# Latitude is DataArr[0], Longitude is DataArr[1], Height is DataArr[2], Slope is DataArr[3]
-def find_min_height(data_arr):
-    return find_min_value(data_arr, 2)
-def find_min_lon(data_arr):
-    return find_min_value(data_arr, 1)
-def find_min_lat(data_arr):
-    return find_min_value(data_arr, 0)
-def find_min_slope(data_arr):
-    return find_min_value(data_arr, 3)
-
-#TODO: new file that puts minimum of lat/long/height/slope and then scale it
-def write_zeroed_file():
+def write_zeroed_file(xmin, ymin, zmin, zeroArray):
     adjusted_path = fc.data_path + "/AdjustedCoordinateData.csv"
-    min_lat = abs(float(find_min_lat(dataArray)))
-    min_lon = abs(float(find_min_lon(dataArray)))
-    min_slope = abs(float(find_min_slope(dataArray)))
-    min_height = abs(float(find_min_height(dataArray)))
     with open(adjusted_path, mode="w", newline="") as f:
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for i in range(len(dataArray)):
-            lunar_rad = (1737.4 * 1000)  # converts provided lunar rad data to meters
-            lat = float(dataArray[i][0]) + min_lat
-            long = float(dataArray[i][1]) + min_lon
-            height = float(dataArray[i][2]) + min_height
-            slope = float(dataArray[i][3]) + min_slope
-            radius = lunar_rad + float(height)
-
-            x = get_x_coord(lat, long, radius)
-            y = get_y_coord(lat, long, radius)
-            z = get_z_coord(lat, long, radius)
-
-            csv_writer.writerow([x, y, z, slope])
+        for i in range(len(zeroArray)):
+            csv_writer.writerow([(zeroArray[i][0] + xmin)/40, (zeroArray[i][1] + ymin)/40,
+                                 (zeroArray[i][2] + zmin)/40, zeroArray[i][3]])
         f.close()
 
 
 
 if __name__ == "__main__":
+    # Latitude is DataArr[0], Longitude is DataArr[1], Height is DataArr[2], Slope is DataArr[3]
     dataArray = []
-    x_and_y_dim = generate_data_array()
+    zeroArray = []
+    x_and_y_dim, data_array_path = generate_data_array()
 
-    # absolute_min_height = find_min_height(dataArray)
-    # absolute_max_height = find_max_height(dataArray)
-    # abs_zero_height_scale = (abs(absolute_max_height) + abs(absolute_min_height))
+    rect_file_path, min_x, min_y, min_z, = write_rect_file(dataArray)
+    adj_file_path = write_zeroed_file(min_x, min_y, min_z, zeroArray)
 
-    rect_file_path = write_rect_file(dataArray)
-    adj_file_path = write_zeroed_file()
-    # misc_path = write_misc_file(absolute_min_height)
-    print("Data Processing Success")
+    # print("Data Processing Success")
