72d4d8b49f7b24fd4be2bcd6d82458754337ace7
Efe Celik <suleymanefeclk2@gmail.com>
2023-03-03 13:08:13 -0800

    Some functions moved to Helpers file

--- FolderCreator.py
+++ FolderCreator.py
@@ -4,38 +4,12 @@ App Development Challenge Application.#
 """
 import os
 import csv
-import tkinter as tk
-from tkinter import messagebox
+
 from shutil import move
 from dotenv import load_dotenv
+from Helpers import find_file, show_error
 
 load_dotenv()
-
-
-def find_file(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-
-
-# Since FolderCreator is used across each file, these helper methods allow
-# creating an error/info/warning popUp in each file.
-def show_error(err_type, msg):
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showerror(err_type, msg)
-
-
-def show_info(title, msg):
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showinfo(title, msg)
-
-
-def show_warning(title, msg):
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showwarning(title, msg)
 
 
 # Helper functions to get file paths from .env
--- Helpers.py
+++ Helpers.py
@@ -1,4 +1,7 @@
 import csv
+import tkinter as tk
+from tkinter import messagebox
+import os
 
 
 def file2list(path):
@@ -8,3 +11,28 @@ def file2list(path):
 
     return new_list
 
+
+def find_file(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+
+# Since FolderCreator is used across each file, these helper methods allow
+# creating an error/info/warning popUp in each file.
+def show_error(err_type, msg):
+    root = tk.Tk()
+    root.withdraw()
+    messagebox.showerror(err_type, msg)
+
+
+def show_info(title, msg):
+    root = tk.Tk()
+    root.withdraw()
+    messagebox.showinfo(title, msg)
+
+
+def show_warning(title, msg):
+    root = tk.Tk()
+    root.withdraw()
+    messagebox.showwarning(title, msg)
