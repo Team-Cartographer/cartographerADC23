25f3d6fe06fb319523f152e4612d892f8060a474
Efe Celik <suleymanefeclk2@gmail.com>
2023-03-04 04:09:50 -0800

    More PEP 8 Style fixes & Unused variabled removed

--- FolderCreator.py
+++ FolderCreator.py
@@ -8,7 +8,6 @@ from shutil import move
 from dotenv import load_dotenv, set_key
 from Helpers import show_error, show_info
 
-
 # .env Loading and Processing
 if not os.path.exists(os.getcwd() + '/.env'):
 
@@ -38,24 +37,29 @@ def get_height_file_path():
 def get_slope_file_path():
     return os.getenv('SLOPE_FILE_PATH').replace("\\", "/")
 
+
 def get_dist_between_points():
     return int(os.getenv('DISTANCE_BETWEEN_POINTS'))
+
 
 def get_size_constant():
     return int(os.getenv("SIZE_CONSTANT"))
 
+
 def get_max_z():
     return int(os.getenv("MAX_Z"))
+
 
 def get_min_z():
     return int(os.getenv('MIN_Z'))
 
+
 def get_min_x():
     return int(os.getenv('MIN_X'))
 
+
 def get_min_y():
     return int(os.getenv('MIN_Y'))
-
 
 
 # IMPORTANT PATHING
@@ -64,8 +68,6 @@ data_path = os.path.join(parent_path, 'Data')
 images_path = os.path.join(data_path, 'Images')
 app_files_path = os.path.join(parent_path, 'App Files')
 archive_path = os.path.join(app_files_path, 'Archived Files')
-dotenv_path = os.path.join(parent_path, '/.env')
-
 
 if __name__ == '__main__':
     set_key('.env', 'SIZE_CONSTANT', '1277')
@@ -76,5 +78,4 @@ if __name__ == '__main__':
         os.mkdir(images_path)
     else:
         show_info('ADC App Installation Update',
-                   "Folder Already Exists on " + parent_path + '\nFiles have been updated.')
-
+                  "Folder Already Exists on " + parent_path + '\nFiles have been updated.')
