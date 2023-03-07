0e0af7c97fafabe5952d8c2481f039df05c22c04
abhi arya <abhia9167@gmail.com>
2023-03-06 16:01:34 -0800

    Fixed Success Messages.

--- Artemis_ADC_Launcher.py
+++ Artemis_ADC_Launcher.py
@@ -1,35 +1,38 @@
 # This program is the central hub that the other programs are run from #
 import os
 from subprocess import run
+from utils import show_info
 
 code_path = os.getcwd()
 
 # runs PathFetcher.exe
-print("Running path fetcher")
+print("Running PathFetcher")
 pathfetcher_path = code_path + "/PathFetcher/PathFetcher.exe"
 pathfetcher_program = run(["cmd", "/c", pathfetcher_path], capture_output=True)
-print("Finished path fetcher")
+print("PathFetcher Success")
 
-print("Running folder creator")
+print("Running FolderCreator")
 # runs FolderCreator.py
 folder_creator_path = code_path + "/FolderCreator.py"
 folder_creator_program = run(["cmd", "/c", folder_creator_path])
-print("Finished folder creator")
+print("FolderCreator Success")
 
-print("Running data processor")
+print("Running DataProcessor")
 # runs DataProcessor.py
 data_processor_path = code_path + "/DataProcessor.py"
 data_processor_program = run(["cmd", "/c", data_processor_path])
-print("Finished data processor")
+print("DataProcessor Success")
 
-print("Running cartographer")
+print("Running Cartographer")
 # runs Cartographer.py
 cartographer_path = code_path + "/Cartographer.py"
 cartographer_program = run(["cmd", "/c", cartographer_path])
-print("Finished cartographer")
+print("Cartographer Success")
 
-print("Running A* (no quad trees)")
+print("Running A* (No QuadTree)")
 # runs A_Star.py (Without quad trees)
 a_star_path = code_path + "/A_Star.py"
 a_star_program = run(["cmd", "/c", a_star_path])
-print("Finished A*")
+print("A* (no QuadTree) Success")
+
+#show_info('Installation Success', 'Please Run Display.py')
