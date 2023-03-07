5156fe93d5f71954b08ec5b29b7abfbed9b061fe
abhi arya <abhia9167@gmail.com>
2023-03-06 17:16:24 -0800

    DISPLAY TENTATIVELY FINISHED (besides Astar), Updated All Comments

--- Display.py
+++ Display.py
@@ -3,19 +3,24 @@ from ursina import *
 from utils import file2list, calc_azimuth_and_elevation, latitude_from_rect, longitude_from_rect, get_radius, height_from_rect, slope_from_rect
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+# Window Declarations and Formatting -------------
 app = Ursina()
 window.fps_counter.disable()
 window.set_title('Team Cartographer\'s ADC Application')
 window.cog_button.disable()
 window.exit_button.color = color.dark_gray
 
-# Display Specific Constants
+# Display Specific Constants -------------
+
 Y_HEIGHT = 128  # Default Value
-RESET_LOC = (0, Y_HEIGHT*8, 0)  # Default PLAYER Value
+RESET_LOC = (0, Y_HEIGHT*8, 0)  # Default PLAYER Positional Value
 SIZE_CONSTANT = fc.get_size_constant()
 EDITOR_SCALE_FACTOR = 3
 PLAYER_SCALE_FACTOR = 8
 
+# Declaration of Entities -------------
+
+# FirstPersonController Ground
 ground_player = Entity(
     model=Terrain(heightmap='processed_heightmap.png'),
     texture='moon_surface_texture.png',
@@ -24,6 +29,7 @@ ground_player = Entity(
     enabled=False
     )
 
+# EditorCamera Ground
 ground_perspective = Entity(
     model=Terrain(heightmap='processed_heightmap.png'),
     texture='moon_surface_texture.png',
@@ -32,6 +38,7 @@ ground_perspective = Entity(
     enabled=False
     )
 
+# EditorCamera Location Beacon
 editor_cam_player_loc = Entity(
     model='cube',
     scale=(20, 1000, 20),
@@ -39,6 +46,7 @@ editor_cam_player_loc = Entity(
     enabled=False,
     )
 
+# Minimap
 minimap = Entity(
     parent=camera.ui,
     center=(0, 0, 0),
@@ -50,6 +58,7 @@ minimap = Entity(
     enabled=False
     )
 
+# Minimap Dot Entity
 mini_dot = Entity(
     parent=minimap,
     model='circle',
@@ -59,10 +68,11 @@ mini_dot = Entity(
     enabled=False
     )
 
+# Slope and Height Toggle Image Pathing -------------
 slopemap = fc.parent_path + '/slopemap.png'
 heightkey = fc.parent_path + '/heightkey.png'
 
-
+# Textboxes  -------------
 t_lat = Text(text='Latitude:', x=-.54, y=.48, scale=1.1, enabled=False)
 t_lon = Text(text='Longitude:', x=-.54, y=.43, scale=1.1, enabled=False)
 t_ht = Text(text='Height:', x=-.54, y=.38, scale=1.1, enabled=False)
@@ -70,47 +80,49 @@ t_slope = Text(text='Slope:', x=-.54, y=.33, scale=1.1, enabled=False)
 t_azi = Text(text='Azimuth:', x=-.54, y=.28, scale=1.1, enabled=False)
 t_elev = Text(text='Elevation:', x=-.54, y=.23, scale=1.1, enabled=False)
 t_pos = Text(text='positional data', x=-0.883, y=0.185, z=0, enabled=False)
-
 t_info = Text(
     text='M for Moon, L for Slopemap, H for Heightmap, Esc for Pause, X for Switch',
     x=-.15, y=-.45, scale=1.1, color=color.black, enabled=False)
 
 
-latitudes = file2list(fc.get_latitude_file_path())
-longitudes = file2list(fc.get_longitude_file_path())
-heights = file2list(fc.get_height_file_path())
-slopes = file2list(fc.get_slope_file_path())
 
+# Player Interactable Declarations -------------
 
 sky = Sky()
 sky.color = '000000' # Black
 
-ec = EditorCamera(enabled=False, zoom_speed=5) # THIS MUST BE INITIALIZED BEFORE <player> OR ZOOMS WON'T WORK.
+ec = EditorCamera(enabled=False, zoom_speed=5) # Note: THIS MUST BE INITIALIZED BEFORE <player> OR ZOOMS WON'T WORK.
 
 player = FirstPersonController(position=RESET_LOC, speed=500, mouse_sensitivity=Vec2(25, 25), enabled=False)
 player.cursor.scale = 0.00000000001 # Hides the Cursor from the App Display
 
-# Shortcuts/Toggle Functions
+
+# Input Functions and Toggles -------------
 def input(key):
 
+    # Reset Player
     if key == 'r':
         player.set_position(RESET_LOC)
 
+    # Slopemap Toggle
     if key == 'l':
         ground_player.texture = 'slopemap.png'
         ground_perspective.texture = 'slopemap.png'
         editor_cam_player_loc.color = color.blue
 
+    # Heightkey Toggle
     if key == 'h':
         ground_player.texture = 'heightkey.png'
         ground_perspective.texture = 'heightkey.png'
         editor_cam_player_loc.color = color.white
 
+    # Moon Texture Toggle (Default)
     if key == 'm':
         ground_player.texture = 'moon_surface_texture.png'
         ground_perspective.texture = 'moon_surface_texture.png'
         editor_cam_player_loc.color = color.red
 
+    # Toggle between Player and EditorCamera
     if key == 'x' and start_bot.enabled is False:
         player.enabled = not player.enabled
         ec.enabled = not ec.enabled
@@ -118,9 +130,11 @@ def input(key):
         ground_perspective.enabled = not ground_perspective.enabled
         editor_cam_player_loc.enabled = not editor_cam_player_loc.enabled
 
+    # Quit App
     if held_keys['left shift', 'q']:
         exit(0)
 
+    # Pause
     if key == 'escape' and pause_bot.enabled is False:
         t_lat.disable()
         t_lon.disable()
@@ -142,6 +156,7 @@ def input(key):
         t_quit.enable()
 
 
+# Game Loop Update() Functions -------------
 def update():
     # Map Failsafe
     if -6150 > player.position.x or player.position.x > 6150 or -6150 > player.position.z or player.position.z > 6150:
@@ -180,12 +195,11 @@ def update():
 
     # Mini-Map Dot Positioning
     mx, mz = (x/12760) + 0.5, (z/12760)-0.5
-    #print(f'({mx}, {mz})')
     mini_dot.position = (mx, mz, 0) # 'z' is treated as 'y' in this case, since 2D objects have no 'y' axis
 
 
 
-# Create Start Menu
+# Create Start Menu -------------
 def start_game():
     ground_player.enable()
     player.enable()
@@ -203,7 +217,7 @@ def start_game():
     mini_dot.enable()
     t_pos.enable()
 
-# Unpause Button Function
+# Unpause Button Function -------------
 def on_unpause():
     ground_player.enable()
     player.enable()
@@ -222,15 +236,18 @@ def on_unpause():
     mini_dot.enable()
     t_pos.enable()
 
-
+# Start Menu Text and Buttons -------------
 t_start_menu = Text(text="Welcome to Team Cartographer's 2023 NASA ADC Application", x=-0.35, y=0.08)
 t_start_menu_creds = Text(text="https://github.com/abhi-arya1/NASA-ADC-App \n \n      https://github.com/pokepetter/ursina", x=-0.275, y=-0.07, color=color.dark_gray)
 start_bot = Button(text='Click to Begin', color=color.gray, highlight_color=color.dark_gray, scale=(0.2, 0.05))
 start_bot.on_click = start_game
 
+# Pause Menu Text and Buttons -------------
 t_pause = Text(text="You are Currently Paused...", x=-0.16, y=0.08, enabled=False)
 pause_bot = Button(text='Click to Unpause', color=color.gray, highlight_color=color.dark_gray, scale=(0.23, 0.05), enabled=False)
 t_quit = Text(text="Press 'LShift+Q' to quit.", x=-0.14, y=-0.06, enabled=False)
 pause_bot.on_click = on_unpause
 
+
+# Runs Display.py -------------
 app.run()
